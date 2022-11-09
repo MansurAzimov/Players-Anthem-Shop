7671ba6b078da6ef1a482d54d66082691cb839f9
MansurAzimov <arkadacity@list.ru>
2022-11-09 16:18:54 +0300

    templates

--- shop/routes.py
+++ shop/routes.py
@@ -9,3 +9,19 @@ def index():
 @app.route('/contact')
 def contact():
     return render_template ('contact.html')
+
+@app.route('/signup')
+def signup():
+    return render_template ('signup.html')
+
+@app.route('/register')
+def register():
+    return render_template ('register.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template ('checkout.html')
+
+@app.route('/products')
+def products():
+    return render_template ('products.html')
