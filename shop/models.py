089a9878a406881854d79f16e9246a67a0860ea4
MansurAzimov <arkadacity@list.ru>
2022-11-16 16:41:05 +0300

    add database

--- shop/models.py
+++ shop/models.py
@@ -8,7 +8,7 @@ class Product(db.Model):
     price = db.Column (db.Integer(),primary_key=False)
     category = db.Column (db.String(),nullable=False)
     size = db.Column (db.Integer,nullable=False)
-    description = db.Column(db.text(),nullable=False)
+    description = db.Column (db.Text(),nullable=False)
     image = db.Column (db.String(),nullable=False)
 
     def __repr__(self) -> str:
